537b344530950128ca2b61ed4c05166fd64cf171
Maksim <106427027+Scrait@users.noreply.github.com>
2024-09-08 09:29:39 +0300

    Update shims-vue.d.ts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,19 +3,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
-declare global {
-  interface Window {
-    turnstile: {
-      render: (
-        elementId: string,
-        options: {
-          sitekey: string,
-          callback: (token: string) => void;
-        }
-      ) => void;
-    };
-  }
-}
-
-export {};

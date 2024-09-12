e22757eba0d7aa4ff9e9ff5bc3a62cb2739277a9
SpaceScout <nelindanila7@gmail.com>
2024-09-12 19:27:09 +0300

    sosy

--- src/components/Reviews/SingleReview.vue
+++ src/components/Reviews/SingleReview.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="review flex flex-col items-center justify-center p-4 bg-custom-black-adventage rounded-xl shadow-lg w-[600px] h-fit text-custom-white-adventage bg-opacity-50 px-6 py-[19.2px] gap-y-4 border-bottom-custom-adventage z-50 relative">
+    <div class="review flex flex-col items-center justify-center p-4 bg-custom-black-adventage rounded-xl shadow-lg lg:w-[600px] w-[300px] min-h-[150px] text-custom-white-adventage bg-opacity-50 px-6 py-[19.2px] gap-y-4 border-bottom-custom-adventage z-50 relative">
         <div class="flex flex-row w-full gap-x-4">
             <img src="@/assets/img/ReviewsAvatars/test.jpeg" alt="User Avatar" class="w-10 h-10 rounded-full ring-2 ring-[#D4CDFF] ring-offset-4 ring-offset-[#09090D] shadow-custom">
             <div class="flex flex-row items-center">
